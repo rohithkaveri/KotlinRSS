7272f5c5c0f5c926aef1f8a8f73ba121af2a73de
rohithkaveri <rohithkaveri5@gmail.com>
2019-07-22 22:21:22 -0700

    added url links

--- app/src/main/java/com/example/kotlinrss/MainAdapter.kt
+++ app/src/main/java/com/example/kotlinrss/MainAdapter.kt
@@ -1,13 +1,18 @@
 package com.example.kotlinrss
 
+import android.content.Intent
+import android.net.Uri
 import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_row.view.*
+
+private var WebUrl:String = "";
 
 class MainAdapter (val items:Items) : RecyclerView.Adapter<MainViewHolder>(){
     override fun getItemCount(): Int {
@@ -34,10 +39,18 @@ class MainAdapter (val items:Items) : RecyclerView.Adapter<MainViewHolder>(){
             categoryTxt += (html + ", ")
         }
 
+
+
         val htmltxt = HtmlCompat.fromHtml(feedItems.description, HtmlCompat.FROM_HTML_MODE_COMPACT)
 
         holder.itemView.textView_categories.apply { text =  categoryTxt}
         holder.itemView.textView_desc.apply { text = htmltxt }
+
+        holder.itemView.setOnClickListener {
+            println(feedItems.link)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(feedItems.link))
+            holder.itemView.context.startActivity(browserIntent)
+        }
 
     }
 
@@ -51,4 +64,14 @@ class MainAdapter (val items:Items) : RecyclerView.Adapter<MainViewHolder>(){
 
 }
 
-class MainViewHolder(view:View) : RecyclerView.ViewHolder(view)
+class MainViewHolder(view:View) : RecyclerView.ViewHolder(view){
+    /*init {
+        view.setOnClickListener {
+            //val intent = Intent(view.context, CourseDetailActivity::class.java)
+            println(WebUrl)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(WebUrl))
+            view.context.startActivity(browserIntent)
+
+        }
+    }*/
+}
